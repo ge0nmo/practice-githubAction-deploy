bfbf955e96968d805157a1a74c4f8aab69575573
ge0nmo <ands0927@naver.com>
2022-12-08 13:38:54 +0900

    test

--- src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PracticeGithubActionDeployApplication {
-
-	public static void main(String[] args) {
+public class PracticeGithubActionDeployApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
 	}
-
 }
